fbb1292ade0a9e7ea4fb0d8724c40c4b17376cef
gbrielchaves9 <gbrielchaves9@gmail.com>
2022-10-17 17:30:46 -0300

    first commit

--- javas.js
+++ javas.js
@@ -1,40 +1,63 @@
+let comida;
 function verdinho(seletor){
-    const outro = document.querySelector('.borda');
+    const outro = document.querySelector('.borda .comida ');
     if ( outro !==null)
     {outro.classList.remove('borda')}
     const botao = document.querySelector(seletor);
     botao.classList.toggle('borda');
 
-   
-}
-function verdinho2(seletor2){
-    const outro = document.querySelector('.borda2');
-    if ( outro !==null)
-    {outro.classList.remove('borda2')}
-    const botao = document.querySelector(seletor2);
-    botao.classList.toggle('borda2');
 
-   
-}
-function verdinho3(seletor3){
-    const outro = document.querySelector('.borda3');
-    if ( outro !==null)
-    {outro.classList.remove('borda3')}
-    const botao = document.querySelector(seletor3);
-    botao.classList.toggle('borda3');
+    comida = botao.innerHTML;
 
-   
-}
-
-function fechar(){
-    const mudar = document.querySelector('.pedido');
-    mudar.classList.remove('pedido2')
-
-}
-
-function inferior(){
-    const mudar = document.querySelector('.pedido');
-    mudar.classList.remove('pedido2')
+    vaifechar();
     
 
 }
+let bebida ;
+function verdinho2(seletor){
+    const outro = document.querySelector('.borda2 .bebida ');
+    if ( outro !==null)
+    {outro.classList.remove('borda2')}
+    const botao = document.querySelector(seletor);
+    botao.classList.toggle('borda2');
+
+
+    bebida = botao.innerHTML;
+
+    vaifechar();
+
+
+ 
+}
+let sobremesa;
+
+function verdinho3(seletor){
+    const outro = document.querySelector('.borda3 .sobremesa');
+    if ( outro !==null)
+    {outro.classList.remove('borda3')}
+    const botao = document.querySelector(seletor);
+    botao.classList.toggle('borda3');
+
+
+    sobremesa = botao.innerHTML;
+
+    vaifechar();
+    
+}
+
+
+function vaifechar(){
+
+
+    if ( comida !== undefined){
+        if ( bebida !== undefined){
+            if ( sobremesa !== undefined){
+                const botao = document.querySelector('.continuar');
+                botao.innerHTML = "Fechar pedido" ;
+        }
+    }
+
+}
+}
+
+    
