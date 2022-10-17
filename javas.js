caeb413b166266a8e70de9a10cd91bd81be5ba92
gbrielchaves9 <gbrielchaves9@gmail.com>
2022-10-17 11:07:36 -0300

    3botao

--- javas.js
+++ javas.js
@@ -7,6 +7,24 @@ function verdinho(seletor){
 
    
 }
+function verdinho2(seletor2){
+    const outro = document.querySelector('.borda2');
+    if ( outro !==null)
+    {outro.classList.remove('borda2')}
+    const botao = document.querySelector(seletor2);
+    botao.classList.toggle('borda2');
+
+   
+}
+function verdinho3(seletor3){
+    const outro = document.querySelector('.borda3');
+    if ( outro !==null)
+    {outro.classList.remove('borda3')}
+    const botao = document.querySelector(seletor3);
+    botao.classList.toggle('borda3');
+
+   
+}
 
 function fechar(){
     const mudar = document.querySelector('.pedido');
